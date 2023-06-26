15cb69097f2cb0b4f387d07bc95450972d786cc2
Daniil <d_platonov@internet.ru>
2023-06-26 23:36:58 +0300

    Initial commit

--- src/main/java/com/yourcompany/musicalbumapi/service/SongService.java
+++ src/main/java/com/yourcompany/musicalbumapi/service/SongService.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class SongService {
+public class   SongService {
     private final SongRepository songRepository;
     private final AlbumRepository albumRepository;
 
